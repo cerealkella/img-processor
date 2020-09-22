5fc8beec7c68e0eb887e637feb49a91aac521f60
Justin <kellerjustin@protonmail.com>
2020-09-22 15:44:18 -0500

    version fix

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/img_processor",
-    version="version=0.3.0",
+    version="0.3.0",
     zip_safe=False,
 )
