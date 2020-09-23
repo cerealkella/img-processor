ed86c775e67ba46a7b0027630621787c473e7837
Justin <kellerjustin@protonmail.com>
2020-09-23 10:28:01 -0500

    Bump version: 0.3.0 → 0.4.0

--- img_processor/__init__.py
+++ img_processor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Justin Keller"""
 __email__ = "kellerjustin@protonmail.com"
-__version__ = "__version__ = '0.3.0'"
+__version__ = "__version__ = '0.4.0'"
 
 
 from .img_processor import ImageProcessor  # noqa
