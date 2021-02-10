364be76f9644de23ec2ec5c3010fc2f2fcfaa0e2
Justin <kellerjustin@protonmail.com>
2021-02-10 10:05:46 -0600

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/img_processor",
-    version="0.10.0",
+    version="0.11.0",
     zip_safe=False,
 )
